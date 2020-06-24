2ce8c8837b14dbefd9f4e7d3cc35c470747d90fc
Thomas Edwards <tomedwards@sirma.com>
2020-06-24 13:18:26 +0100

    initial commit

---
+++ src/main/java/com/tedwards/report/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.tedwards.report.repository;
+
+import com.tedwards.report.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
---
+++ src/main/java/com/tedwards/report/service/ReportService.java
@@ -0,0 +1,42 @@
+package com.tedwards.report.service;
+
+import com.tedwards.report.entity.Employee;
+import com.tedwards.report.repository.EmployeeRepository;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path = "C:\\Users\\Thomas\\Desktop\\Report";
+        List<Employee> employees = repository.findAll();
+        File file = ResourceUtils.getFile("classpath:employees.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Thomas Edwards");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\employees.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\employees.pdf");
+        }
+
+        return "report generated in path : " + path;
+    }
+}
